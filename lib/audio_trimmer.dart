e696c4913f78d75ab94d9e51470ff7fa87ed3dd7
meghead <megthing@gmail.com>
2021-07-25 16:27:33 +0100

    Update audio_trimmer.dart

--- lib/audio_trimmer.dart
+++ lib/audio_trimmer.dart
@@ -27,7 +27,7 @@ class AudioTrimmer {
     final Directory dir = await getTemporaryDirectory();
     final outPath = "${dir.path}/output.m4a";
     var cmd =
-        "-y -i \"$path\" -vn -ss $start -to $end -ar 44k -ac 2 -b:a 96k -acodec copy $outPath";
+        "-y -i \"$path\" -vn -ss $start -to $end -ar 44k -c:a aac 2 -b:a 96k -acodec copy $outPath";
     int rc = await _flutterFFmpeg.execute(cmd);
 
     if (rc != 0) {
